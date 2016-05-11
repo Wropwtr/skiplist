41ccdbee6421c12a70dd484e263c7fc8f3c66117
Wropwtr <buldakov.95@mail.ru>
2016-05-11 20:38:16 +0400

    Add more elements for comparsion

--- projects/comparsion/comparsion.pro
+++ projects/comparsion/comparsion.pro
@@ -3,6 +3,8 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
+QMAKE_CXXFLAGS -= -O0
+
 SOURCES += main.cpp
 
 include(deployment.pri)
--- projects/comparsion/main.cpp
+++ projects/comparsion/main.cpp
@@ -6,46 +6,47 @@
 
 using namespace std;
 
-//#define NELEMS 128
-#define NELEMS 10000
-
 int main(){
     volatile int key;
     int *a = new int;
     *a = 0;
     srand(time(NULL));
+    int nums[6] = {128, 512, 2000, 8000, 20000,50000};
+    int levels[6] = {7,9,11,14,15,16};
+    for (int k = 0; k < 6; k++){
+        int NELEMS = nums[k];
+        List<int> *list = new List<int>();
+        for (int i = 0; i < NELEMS; i++){
+            list->ins(i, a);
+        }
+        clock_t begin = clock();
+        for (int i = 0; i < 10000; i++){
+            key = ((int)rand() % NELEMS);
+            list->search(key);
+        }
+        clock_t end = clock();
+        double elapsed_secs_list = double(end - begin)/10000/CLOCKS_PER_SEC;
 
-    List<int> *list = new List<int>();
-    for (int i = 0; i < NELEMS; i++){
-        int r = rand();
-        list->ins(r, a);
-    }
-    clock_t begin = clock();
-    for (int i = 0; i < 1000; i++){
-        key = ((int)rand() % NELEMS);
-        list->search(key);
-    }
-    clock_t end = clock();
-    double elapsed_secs_list = double(end - begin) / CLOCKS_PER_SEC;
+        SkipList<int> *slist = new SkipList<int>(levels[k], 0.5f);
+        for (int i = NELEMS; i > 0; i--){
+            slist->insert(i, a);
+        }
+        begin = clock();
+        for (int i = 0; i < 10000; i++){
+            key = ((int)rand() % NELEMS);
+            slist->find(key);
+        }
+        end = clock();
+        double elapsed_secs_slist = double(end - begin)/10000/CLOCKS_PER_SEC;
+        cout << "For " << NELEMS << " elements" << endl;
+        cout << "list search time:     ";
+        printf("%0.8lf\n",elapsed_secs_list);
+        cout << "skiplist search time: ";
+        printf("%0.8lf\n",elapsed_secs_slist);
 
-    SkipList<int> *slist = new SkipList<int>(18, 0.5f);
-    for (int i = 0; i < NELEMS; i++){
-        int r = rand();
-        slist->insert(r, a);
+        delete list;
+        delete slist;
+        delete a;
     }
-    begin = clock();
-    for (int i = 0; i < 1000; i++){
-        key = ((int)rand() % NELEMS);
-        slist->find(key);
-    }
-    end = clock();
-    double elapsed_secs_slist = double(end - begin) / CLOCKS_PER_SEC;
-    cout << "For " << NELEMS << " elements" << endl;
-    cout << "list search time: " << elapsed_secs_list << endl;
-    cout << "skiplist search time: " << elapsed_secs_slist << endl;
-    cout << endl;
-
-    delete list;
-    delete slist;
-    delete a;
+    cout << "\n[ms]";
 }
